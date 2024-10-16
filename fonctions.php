f50d8b680e009d4fba44d29419f4d10865abdab3
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-10-16 13:53:59 -0400

    s2c1 Debut structure de theme

---
+++ fonctions.php
@@ -0,0 +1,19 @@
+<?php
+// Ajouter le style sur la page
+// Ajouter le link css dans la page 
+// Premier qui s'execute !
+function ajouter_style()
+{
+    // pour etre capable d'ajouter une feuille de style -> nécessaire action 
+    wp_enqueue_style(
+        'mon_stlyle',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        filemtime(get_template_directory() . '/style.css')
+    );
+}
+
+// Le hook 
+// add_action= l'équivalent en JS add.event.listerner = écouteur de WordPress
+// wp_enqueue_scripts = fonctin du script et ajouter_style = execute
+add_action('wp_enqueue_scripts', 'ajouter_style');
