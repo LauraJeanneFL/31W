5174d69d2700bb0f220050d0243d2369e05847bf
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-10-17 18:43:33 -0400

    Dernières modifications scss

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="pied">
     <section class="global">
       <div>Auteur: Laura-Jeanne Fournier Lanctôt</div>
-      <div>2</div>
-      <div>3</div>
+      <div>17 octobre 2024</div>
+      <div></div>
     </section>
   </footer>
   <?php wp_footer(); ?>
