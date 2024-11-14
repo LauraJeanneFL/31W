a9a2180db21869b6b092e59682481feaee263a20
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-11-14 11:12:29 -0500

    correction et ajout de code JS

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,28 +1,56 @@
 (function () {
   console.log("Vive JavaScript");
+
   let carrousel__bouton = document.querySelector(".carrousel__bouton");
   let carrousel__x = document.querySelector(".carrousel__x");
   let carrousel = document.querySelector(".carrousel");
-  let carrousel__image = document.querySelector(".carrousel__image");
   let carrousel__figure = document.querySelector(".carrousel__figure");
-  let galerie = document.querySelector(".galerie");
   let galerie__img = document.querySelectorAll(".galerie img");
-  console.log(galerie__img.length);
+
+  let currentIndex = 0;
+
+  // Fonction pour remplir le carrousel avec les images de la galerie
   function remplirCarrousel() {
-    for (elm of galerie__img) {
-      console.log(elm.src);
+    carrousel__figure.innerHTML = "";
+    galerie__img.forEach((elm, index) => {
       let img = document.createElement("img");
       img.src = elm.src; // copie une image de la galerie vers le carrousel
       img.classList.add("carrousel__img");
+      if (index !== currentIndex) {
+        img.classList.add("carrousel__img--hidden");
+      }
       carrousel__figure.appendChild(img);
-    }
+    });
+  }
+
+  // Affiche l'image à l'index spécifié
+  function afficheImage(index) {
+    let images = document.querySelectorAll(".carrousel__img");
+    images.forEach((img, i) => {
+      img.classList.toggle("carrousel__img--visible", i === index);
+      img.classList.toggle("carrousel__img--hidden", i !== index);
+    });
+    currentIndex = index;
+  }
+
+  // Navigation pour passer à l'image suivante
+  function imageSuivante() {
+    currentIndex = (currentIndex + 1) % galerie__img.length;
+    afficheImage(currentIndex);
+  }
+
+  // Navigation pour passer à l'image précédente
+  function imagePrecedente() {
+    currentIndex =
+      (currentIndex - 1 + galerie__img.length) % galerie__img.length;
+    afficheImage(currentIndex);
   }
 
   carrousel__bouton.addEventListener("click", function () {
     if (carrousel__figure.innerHTML === "") {
       remplirCarrousel();
     }
-    afficheImage(4);
+    afficheImage(currentIndex);
     carrousel.classList.add("carrousel--ouvrir");
     console.log("Ouvrir");
   });
@@ -32,11 +60,10 @@
     console.log("fermer");
   });
 
-  function afficheImage(index) {
-    let carrousel__img = document.querySelectorAll(".carrousel__img");
-    for (let i = 0; i < carrousel__img.length; i++) {
-      carrousel__img[i].classList.remove("carrousel__img--visible");
-    }
-    carrousel__img[index].classList.add("carrousel__img--visible");
-  }
+  document
+    .querySelector(".carrousel__droite")
+    .addEventListener("click", imageSuivante);
+  document
+    .querySelector(".carrousel__gauche")
+    .addEventListener("click", imagePrecedente);
 })();
