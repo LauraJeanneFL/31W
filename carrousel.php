3dde01c0285b70acb58d6556dd71933ea87f4aa5
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-11-14 10:06:31 -0500

    correction du code php

--- carrousel.php
+++ carrousel.php
@@ -12,14 +12,14 @@ function enfile_css_js()
 
     wp_enqueue_style(
         "carrousel",
-        plugin_dir_url(__FILE__) . "/style.css",
+        plugin_dir_url(__FILE__) . "style.css",
         array(),
         $version_css
     );
 
     wp_enqueue_script(
         "carrousel",
-        plugin_dir_url(__FILE__) . "/js/carrousel.js",
+        plugin_dir_url(__FILE__) . "js/carrousel.js",
         array(),
         $version_js,
         true
