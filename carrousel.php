61453f9e3c610cc927caa651a72f6966bd5f9b42
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-11-14 09:32:48 -0500

    s5c2 ajout des dossiers

---
+++ carrousel.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin name: Carrousel
+Description: Carrousel permettant d'afficher le contenu d'une galerie
+author: Laura-Jeanne Fournier L.
+author uri: http://referenced.ca
+*/
+function enfile_css_js()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__). "/style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__). "/js/carrousel.js");
+
+    wp_enqueue_style(
+        "carrousel",
+        plugin_dir_url(__FILE__) . "/style.css",
+        array(),
+        $version_css
+    );
+
+    wp_enqueue_script(
+        "carrousel",
+        plugin_dir_url(__FILE__) . "/js/carrousel.js",
+        array(),
+        $version_js,
+        true
+    );
+}
+add_action("wp_enqueue_scripts", "enfile_css_js");
+
+
+function genere_carrousel() {
+    $chaine = 
+        '
+        <button class="carrousel__bouton">Ouvrir</button>
+        <div class="carrousel">
+            <button class="carrousel__x">X</button>
+            <button class="carrousel__gauche">Gauche</button>
+            <button class="carrousel__droite">Droite</button>
+            <figure class="carrousel__figure"></figure>
+            <form class="carrousel__form"></form>
+        </div>
+        ';
+    return $chaine;
+}
+
+add_shortcode("carrousel", "genere_carrousel");
+
