febde6da39297ff0a5ff6d7667bbb9ac8b45d6d0
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-17 15:13:03 -0500

    Modifier le searchform

--- searchform.php
+++ searchform.php
@@ -5,6 +5,6 @@
     <label for="search-field" class="screen-reader-text">Rechercher :</label>
     <input id="search-field" class="recherche__input" type="search" placeholder="Recherche :" value="<?php echo get_search_query(); ?>" name="s" />
     <button class="recherche__bouton" type="submit">
-        <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20" alt="Icone de recherche">
+        <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20" alt="Icône de recherche">
     </button>
 </form>
