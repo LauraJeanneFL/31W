0e8451f0a5e9fbc86c3c4499c80ab0192e307b63
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-19 11:06:12 -0500

    Corriger section bouton

--- page.php
+++ page.php
@@ -15,6 +15,14 @@
           <article class="principal__article">
             <h5><?php the_title() ?></h5>
             <p><?php the_content() ?></p>
+            <div class="filters">
+                <?php
+                $pays = ["France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse"];
+                foreach ($pays as $pays_nom) {
+                    echo '<button class="pays-button" data-pays="' . esc_attr($pays_nom) . '">' . esc_html($pays_nom) . '</button>';
+                }
+                ?>
+            </div>
           </article>
         <?php endwhile; ?>
     </div>
