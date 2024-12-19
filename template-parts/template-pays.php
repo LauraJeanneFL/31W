42343d78f9d181325ea892527527dac6710d43df
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-19 13:45:23 -0500

    Ajout d'une class css

--- template-parts/template-pays.php
+++ template-parts/template-pays.php
@@ -10,41 +10,41 @@ get_header();
     <section class="global">
         <h2>Destinations par Pays</h2>
         <div class="principal__conteneur">
-            <p>
-                Voici une liste de destinations par pays pour votre prochain séjour Vous pouvez cliquer sur les boutons pour afficher les images correspondantes.
-                N'hésitez pas à contacter le service clientèle si vous avez besoin d'aide ou si vous avez des questions sur une destination spécifique.
-                <br>
-                Nous sommes là pour vous aider et vous orienter vers la destination la plus appropriée pour votre budget et vos préférences.
+            <article class="conteneur__centre">
                 <p>
-                    <a href="<?php echo esc_url(home_url('/contact'));?>">Contactez-nous</a>
+                    Voici une liste de destinations par pays pour votre prochain séjour Vous pouvez cliquer sur les boutons pour afficher les images correspondantes.
+                    N'hésitez pas à contacter le service clientèle si vous avez besoin d'aide ou si vous avez des questions sur une destination spécifique.
+                    <br>
+                    Nous sommes là pour vous aider et vous orienter vers la destination la plus appropriée pour votre budget et vos préférences.
+                    <p>
+                        <a href="<?php echo esc_url(home_url('/contact'));?>">Contactez-nous</a>
+                    </p>
                 </p>
-            </p>
-            
-            <!-- Section pour les boutons des pays -->
-            <div class="filters">
-                <h3>Choisissez un pays</h3>
-                <div class="countries">
-                    <?php
-                    $pays = ["France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse"];
-                    foreach ($pays as $pays_nom) :
-                        echo '<button class="pays-button" data-pays="' . esc_attr($pays_nom) . '">' . esc_html($pays_nom) . '</button>';
-                    endforeach;
-                    ?>
+                
+                <!-- Section pour les boutons des pays -->
+                <div class="filters">
+                    <h3>Choisissez un pays</h3>
+                    <div class="countries">
+                        <?php
+                        $pays = ["France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse"];
+                        foreach ($pays as $pays_nom) :
+                            echo '<button class="pays-button" data-pays="' . esc_attr($pays_nom) . '">' . esc_html($pays_nom) . '</button>';
+                        endforeach;
+                        ?>
+                    </div>
                 </div>
-            </div>
-
-            <!-- Section pour la galerie -->
-            <div class="galerie-container">
-                <h2>Galerie d'images</h2>
-                <div class="images">
-                    <?php get_template_part('template-parts/customizer-galerie'); ?>
+                <!-- Section pour la galerie -->
+                <div class="galerie-container">
+                    <h2>Galerie d'images</h2>
+                    <div class="images">
+                        <?php get_template_part('template-parts/customizer-galerie'); ?>
+                    </div>
                 </div>
-            </div>
-
-            <!-- Conteneur pour afficher les résultats -->
-            <div id="resultats" class="resultats">
-                <p>Faites un choix. Les destinations s'afficheront ici après votre sélection.</p>
-            </div>
+                <!-- Conteneur pour afficher les résultats -->
+                <div id="resultats" class="resultats">
+                    <p>Faites un choix. Les destinations s'afficheront ici après votre sélection.</p>
+                </div>
+            </article>
 
         </div>
     </section>
