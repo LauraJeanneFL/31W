5c95234928994510187f53a307078d4ff7d7e1aa
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-19 12:33:59 -0500

    Ajout d'un customiser-galerie

--- template-parts/customizer-galerie.php
+++ template-parts/customizer-galerie.php
@@ -1,19 +1,19 @@
 <?php
-// Récupèrer les URLs des images depuis le Customizer
+/*
+Customize Name: customizer-galerie
+*/
 $galerie_images = get_theme_mod('galerie_images', '');
 
-// Vérifier si des images sont définies
-if ($galerie_images) :
-    // Convertir les URLs en tableau
-    $images = array_filter(explode("\n", trim($galerie_images))); 
-    ?>
-    <div class="galerie-container">
-        <?php foreach ($images as $image) : ?>
-            <div class="galerie-item">
-                <img src="<?php echo esc_url($image); ?>" alt="Galerie Image">
-            </div>
-        <?php endforeach; ?>
-    </div>
-<?php else : ?>
-    <p>Aucune image n'a été ajoutée à la galerie pour le moment.</p>
-<?php endif; ?>
+// Vérifie si des images sont définies
+if ($galerie_images) {
+    $images = explode("\n", trim($galerie_images));
+    echo '<div class="galerie-container">';
+    foreach ($images as $image) {
+        echo '<div class="galerie-item">';
+        echo '<img src="' . esc_url($image) . '" alt="Galerie Image">';
+        echo '</div>';
+    }
+    echo '</div>';
+} else {
+    echo '<p>Aucune image n\'a été ajoutée à la galerie pour le moment.</p>';
+}
